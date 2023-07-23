13b86224fc172aa047cde8a5fae322c8e3aa3ddf
jerry <jerry_cj_liu@wistron.com>
2023-07-23 22:25:18 +0800

    feat: spare backend and frontend folder

--- dist/modules/users/user.input.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class UserInput {
-    name: string;
-    email: string;
-    password: string;
-    phone?: string;
-    picture: string;
-    company?: string;
-    friendIds: number[];
-}
